49766d6cc5016b2aec88a202c459c949db52f43a
LcLz0 <netharion@gmail.com>
2020-09-23 15:01:35 +0200

    Added doc for useful resources

---
+++ ex_uppgift1/create_user.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+###################################################################################################
+# Script will read names from a text file, change them to a more linuxfriendly format, and create
+# a user with that name and pw Linux4Ever. Script should be general for any lenght of text file.
+###################################################################################################
