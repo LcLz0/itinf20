86f3d6fb0fb3deb3d233d3899b67e929941abb69
lz0 <netharion@gmail.com>
2020-09-24 16:39:52 +0000

    Fully functional script! v1.0

--- ex_uppgift1/create_user.sh
+++ ex_uppgift1/create_user.sh
@@ -17,14 +17,14 @@ fi
 
 # Declare variables that will be used through script.
 file_len=$(wc -l $1 | cut -d ' ' -f 1)
-file_list=$1
+file_name=$1
+passw=$(perl -e 'print crypt($ARGV[0], "password")' Linux4Ever)
 
-for (( i=1; i<$file_len; i++ )) ; do
-	user_name=$(awk -v ite=$i 'NR==ite' $file_list)		 # Read username
+for (( i=1; i<=$file_len; i++ )) ; do
+	user_name=$(awk -v ite=$i 'NR==ite' $file_name)		 # Read username
 	user_name=$(echo $user_name | awk '{print tolower($0)}') # Change username to lowercase
 	f_name=$(echo $user_name | cut -c 1)			 # Cut first char of first name
 	l_name=$(echo $user_name | cut -d ' ' -f 2)		 # Cut whole last name
 	user_name=$f_name$l_name				 # Add first char to last name
-	echo $user_name
-	
+	useradd -p $passw $user_name
 done
