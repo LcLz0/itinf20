28ec22c8d7f3679d3834aaa20ff7d2e5a024b699
lz0 <netharion@gmail.com>
2020-09-24 11:33:58 +0000

    Clean now, using variables instead of commands in loop

--- ex_uppgift1/for_test1.sh
+++ ex_uppgift1/for_test1.sh
@@ -3,10 +3,10 @@
 # Declare some variables 
 # Need file passed as arg. Add control for that later on
 name_file=${1}
-#file_len=$(wc -l ${name_file})
+file_len=$(wc -l ${name_file} | cut -d ' ' -f 1)
 
 #echo $file_len
 
-for i in $(wc -l ${name_file} | cut -d ' ' -f 1) ; do
+for i in $file_len ; do
 	echo "Welcome $i times"
 done
