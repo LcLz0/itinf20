bcf99ea021c1acc6cebc78100e433eab03c4bcb2
lz0 <netharion@gmail.com>
2020-09-24 13:30:53 +0000

    New for testing, got working awk command

---
+++ ex_uppgift1/for_test2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+name_file=${1}
+file_len=$(wc -l ${name_file} | cut -d ' ' -f 1)
+
+#echo $file_len
+
+for (( i=1; i<=$file_len; i++ )) ; do
+	awk -v ite=$i 'NR==ite' $name_file
+done
+
+#for i in {1..$file_len} ; do
+#	echo $i
+#done
