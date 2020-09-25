e230fd6baec6ffee65877f2133e9965b0ea5a561
LcLz0 <netharion@gmail.com>
2020-09-25 13:48:16 +0200

    First version of logger, output both to file and stdout

---
+++ ovningar/logger.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Define logfile
+log_file=logger.log
+
+# Define logger function
+logger(){
+	timestamp=$(date +%Y-%m-%d\ %H:%M:%S)
+	script=$0
+	#severity=INFO
+	echo "${timestamp} ${script} ${@} "
+        1>> $log_file	
+}
+
+logger.info(){
+	logger INFO: $*
+}
+
+logger.warning(){
+	logger WARNING: $*
+}
+
+logger.error(){
+	logger ERROR: $*
+}
+
+logger.info "Starting script"
+logger.warning "Heavy calculation"
+sleep 3
+logger.error "Calculation complete"
