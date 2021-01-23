0a13725961141b1d145c304cf7147cd4f4c84bfb
LcLz0 <netharion@gmail.com>
2021-01-23 13:27:10 +0100

    First working version of calculator.sh
    I've got a fully working version of calculator here. However,
    double quotes is a must when calling from command line. Everything
    else works as expected. See this as a First Working Commit

--- linux2/extra1/calculator.sh
+++ linux2/extra1/calculator.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+# Grep for arithmetic operator
+operator=$(echo "${@}" |egrep -o '(\+|\-|\*|\/)')
+
+# Get numbers
+num1=$(echo $@ | cut -d "${operator}" -f 1)
+num2=$(echo $@ | cut -d "${operator}" -f 2)
+
+echo $((${num1}${operator}${num2}))
