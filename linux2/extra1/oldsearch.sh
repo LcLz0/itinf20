239e96eadad54349e3f781694de93df9b7dd57be
LcLz0 <netharion@gmail.com>
2021-01-23 12:25:26 +0100

    Added check to only delete reg files, not dirs

--- linux2/extra1/oldsearch.sh
+++ linux2/extra1/oldsearch.sh
@@ -5,7 +5,7 @@ checktime=$(($nixtime-172800))
 
 for i in $(ls $pwd); do
 	file_ctime=$(/usr/bin/stat -c %W $i)
-	if [ $file_ctime -lt $checktime ]; then
+	if [ $file_ctime -lt $checktime ] && [ -f $i ] ; then
 		echo "Deleting $i"
 	fi
 done
