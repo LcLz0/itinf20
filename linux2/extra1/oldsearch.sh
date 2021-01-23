6d6335640c6f3271ecbc0a3e41cd3c94bf4b70af
LcLz0 <netharion@gmail.com>
2021-01-23 12:42:09 +0100

    Added confirmation check for file deletion

--- linux2/extra1/oldsearch.sh
+++ linux2/extra1/oldsearch.sh
@@ -6,6 +6,13 @@ checktime=$(($nixtime-172800))
 for i in $(ls $pwd); do
 	file_ctime=$(/usr/bin/stat -c %W $i)
 	if [ $file_ctime -lt $checktime ] && [ -f $i ] ; then
-		echo "Deleting $i"
+
+		echo "Found $i. Are you sure you want to delete? y or n"
+		read choice_check 
+		if [ $choice_check == y ]; then
+			echo "Deleting $i"
+		else
+			echo "Ignoring $i"
+		fi
 	fi
 done
