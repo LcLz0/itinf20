29625fbf6868b313b7a508ee35971d3db4434dc7
LcLz0 <netharion@gmail.com>
2021-01-23 13:35:40 +0100

    Added short comment, cleanup of egrep regex

--- linux2/extra1/calculator.sh
+++ linux2/extra1/calculator.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Grep for arithmetic operator
-operator=$(echo "${@}" |egrep -o '(\+|\-|\*|\/)')
+operator=$(echo "${@}" |egrep -o '(\+|-|\*|\/)')
 
 # Get numbers
 num1=$(echo $@ | cut -d "${operator}" -f 1)
 num2=$(echo $@ | cut -d "${operator}" -f 2)
 
+# Perform calculation
 echo $((${num1}${operator}${num2}))
