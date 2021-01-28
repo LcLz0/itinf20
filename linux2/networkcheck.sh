c5414a48011c46d560c793365580c1ae90058af8
Lz0 <netharion@gmail.com>
2021-01-28 10:26:41 +0100

    removed while

--- linux2/networkcheck.sh
+++ linux2/networkcheck.sh
@@ -4,8 +4,6 @@ interface=eno1
 date=$(date +%Y:%m:%d_%H:%M:%S)
 logfile=/home/lz0/networkcheck.log
 
-while true; do
 	if [ ! $(cat /sys/class/net/${interface}/operstate) == "up" ] ; then
 		echo "[WARNING] -  $date - $interface is down!" >> $logfile
 	fi
-done
