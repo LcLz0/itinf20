34b2d83d669db38a8b4e4a836065998e00c77cc4
Lz0 <netharion@gmail.com>
2021-01-28 10:24:31 +0100

    Formatting of log

--- linux2/networkcheck.sh
+++ linux2/networkcheck.sh
@@ -5,6 +5,6 @@ date=$(date +%Y:%m:%d_%H:%M:%S)
 
 while true; do
 	if [ ! $(cat /sys/class/net/${interface}/operstate) == "up" ] ; then
-		echo "[WARNING] $date $interface is down!"
+		echo "[WARNING] -  $date - $interface is down!"
 	fi
 done
